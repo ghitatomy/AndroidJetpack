18bb008e84ab7049e618760a800fc4b9a1033072
Ghita Tomoiaga <ghita@luminos.software>
2019-09-02 12:27:17 +0300

    Add data binding

--- app/src/main/java/home/com/MainActivity.kt
+++ app/src/main/java/home/com/MainActivity.kt
@@ -1,12 +1,17 @@
 package home.com
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import home.com.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.tvName.text = "Dan Brown"
+        binding.tvEmail.text = "danbrown@gmail.com"
     }
 }
