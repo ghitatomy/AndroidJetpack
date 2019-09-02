62a5f4b0e0159460c5f8da8da81af85d1f240d7f
Ghita Tomoiaga <ghita@luminos.software>
2019-09-02 14:46:45 +0300

    Lifecycle Aware Components

--- app/src/main/java/home/com/MainActivity.kt
+++ app/src/main/java/home/com/MainActivity.kt
@@ -3,6 +3,7 @@ package home.com
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import home.com.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +18,38 @@ class MainActivity : AppCompatActivity() {
         binding.handler = EventHandler(this)
 
         binding.imageUrl = "https://i.redd.it/lhw4vp5yoy121.jpg"
+
+        Log.i(TAG, "Owner onCreate ");
+
+        lifecycle.addObserver(MainActivityObserver())
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "Owner onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "Owner onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "Owner onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "Owner onDestroy")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "Owner onStop")
+    }
+
+    companion object {
+        private val TAG: String = MainActivity::class.java.simpleName
     }
 }
